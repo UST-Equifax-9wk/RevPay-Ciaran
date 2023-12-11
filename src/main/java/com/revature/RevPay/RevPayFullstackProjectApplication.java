7b9ecd7ab002a0457c4e0a8a4fdf18f0a05fc3e1
Cyaorn <diepc153@gmail.com>
2023-12-11 15:58:33 -0600

    adding boilerplate codefrom start.spring.io, editing gitignore for properties file also

---
+++ src/main/java/com/revature/RevPay/RevPayFullstackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevPayFullstackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevPayFullstackProjectApplication.class, args);
+	}
+
+}
