3a5e8477b7cfa2135c8378b3c80cc8f61a17df92
Cyaorn <diepc153@gmail.com>
2023-12-27 17:17:39 -0500

    added InsufficientBalanceException for future transaction implementation

---
+++ revpay-server/src/main/java/com/revature/RevPay/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package com.revature.RevPay.exceptions;
+
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
